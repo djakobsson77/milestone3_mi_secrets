74de25499f2d75ac9cd0cddecde322a026ff626f
djakobsson77 <djakobsson77@gmail.com>
2026-05-02 11:26:50 +0200

    Fixing the last details to get contact form to work. User story completed

--- mi_secrets/settings.py
+++ mi_secrets/settings.py
@@ -116,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Email backend for development
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
